82c4136a981cd00239178965a3a19f02ea361160
Brysen LaBute <blabute@appliedsystems.com>
2017-07-07 09:25:42 -0400

    upgrade to SQL Server 2016 and remove security scripts as this needs to be handled in OctopusDeploy

--- LearnerRaterDB/Security/LearnerRater_Login.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [LearnerRater]
-    WITH PASSWORD = N'E2WvvS{VyCwypiutioramT5cmsFT7_&#$!~<{J_jvdy|qeye', SID = 0xCB98DB5018BFB649AC57EA6BE745A136, DEFAULT_DATABASE = [LearnerRater], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
-
